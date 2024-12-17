2c11d4b059755ceed6543f4e9f9d34bf4f3cfe60
gocallum <callumbir@gmail.com>
2024-12-18 00:17:37 +1100

    added given_name and lastname in registration

--- app/components/AuthProvider.tsx
+++ app/components/AuthProvider.tsx
@@ -11,5 +11,16 @@ function AuthProviderComponent({ children }: { children: React.ReactNode }) {
   return <>{children}</>;
 }
 
-export const AuthProvider = withAuthenticator(AuthProviderComponent);
+export const AuthProvider = withAuthenticator(AuthProviderComponent, {
+  signUpAttributes: ['given_name', 'family_name'],
+  formFields: {
+    signUp: {
+      given_name: { label: 'First Name', placeholder: 'Enter your first name', isRequired: true },
+      family_name: { label: 'Last Name', placeholder: 'Enter your last name', isRequired: true },
+      //email: { label: 'Email', type: 'email', isRequired: true },
+      password: { label: 'Password', type: 'password', isRequired: true },
+      confirm_password: { label: 'Confirm Password', type: 'password', isRequired: true },
+    },
+  },
+});
 
